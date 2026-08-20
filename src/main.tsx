25042eeadbd8fe8c7dec462e1066fa386beade76
Hemispere <andreyfork@mail.ru>
2026-08-20 14:54:50 +0700

    чиним метрику

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import { initMetrika } from './lib/metrika'
-
-initMetrika()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
